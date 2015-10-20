25c2f568f71f10742c9be1394579572f4b584c89
Drew <drewmcdermott@comcast.net>
2015-10-19 23:53:39 -0400

    Starting URL paths

--- mafia_app/views.py
+++ mafia_app/views.py
@@ -6,12 +6,44 @@ from mafia_app.models import Game
 def index(request):
     return  render(request, 'index.html', {})
 
+
+#game pages
 def game(request, game):
     try:
-        game = Game.objects.get(gameName=game)
+        gameI = Game.objects.get(gameName=game)
     except Game.DoesNotExist:
         raise Http404('you didn\'t say the magic word')
     return render(request, 'game/index.html',
     {
-    'game':game,
+    'game':gameI,
+    })
+
+def players(request, game):
+    try:
+        gameI = Game.objects.get(gameName=game)
+    except Game.DoesNotExist:
+        raise Http404('you didn\'t say the magic word')
+    return render(request, 'game/players.html',
+    {
+    'game':gameI,
+    })
+
+def comments(request, game):
+    try:
+        gameI = Game.objects.get(gameName=game)
+    except Game.DoesNotExist:
+        raise Http404('you didn\'t say the magic word')
+    return render(request, 'game/comments.html',
+    {
+    'game':gameI,
+    })
+
+def phases(request, game):
+    try:
+        gameI = Game.objects.get(gameName=game)
+    except Game.DoesNotExist:
+        raise Http404('you didn\'t say the magic word')
+    return render(request, 'game/phases.html',
+    {
+    'game':gameI,
     })
--- mafia_toolkit/urls.py
+++ mafia_toolkit/urls.py
@@ -13,4 +13,14 @@ urlpatterns = [
 
     #Main game dashboard
     url(r'^g/(?P<game>\S+)/', views.game, name='game'),
+
+    #List players in game
+    url(r'^g/(?P<game>\S+)/players', views.players, name='players'),
+
+    #list phases in a game
+    url(r'^g/(?P<game>\S+)/phases', views.phases, name='phases'),
+
+    #List comments in a game
+    url(r'^g/(?P<game>\S+)/phases', views.comments, name='comments'),
+
 ]
